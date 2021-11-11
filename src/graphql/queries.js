e01bcad5a5d7f94cb58b91544b5b9a4f6a3a34aa
Davron <davron_sattorov_99@mail.ru>
2021-11-11 02:09:44 +0200

    GraphQL queries implemented

---
+++ src/graphql/queries.js
@@ -0,0 +1,75 @@
+import { gql } from "@apollo/client";
+
+export const LOAD_DATA = gql`
+  query {
+    categories {
+      name
+      products {
+        id
+        name
+        inStock
+        prices {
+          amount
+          currency
+        }
+        gallery
+      }
+    }
+  }
+`;
+
+export const GET_CATEGORIES = gql`
+  query {
+    categories {
+      name
+    }
+  }
+`;
+
+export const GET_PRODUCTS_BY_CATEGORY = (input) => gql`
+query {
+  category(input: {title: "${input}"}) {
+    name
+    products {
+      id
+      name
+      inStock
+      prices {
+        amount
+        currency
+      }
+      category
+      gallery
+    }
+  }
+}
+`;
+
+export const GET_PRODUCT_BY_ID = (input) => gql`
+query {
+  product(id:"${input}") {
+    id
+    name
+    prices{
+      amount
+      currency
+    }
+    attributes{
+      name
+      items{
+        id
+        value
+      }
+    }
+    category
+    gallery
+    description
+  }
+}
+`;
+
+export const GET_CURRENCIES = () => gql`
+  query {
+    currencies
+  }
+`;
